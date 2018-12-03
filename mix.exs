672ec9d8fb6c5a047216026de9ad4dfcd681afb9
Leonid Batyuk <leonid.batyuk@meltwater.com>
2018-12-03 16:21:36 +0100

    Fix formatting in mix.exs to reduce compiler warnings

--- mix.exs
+++ mix.exs
@@ -2,17 +2,19 @@ defmodule Addict.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :addict,
-     version: "0.3.0",
-     elixir: "~> 1.2",
-     description: description,
-     package: package,
-     docs: &docs/0,
-     deps: deps]
+    [
+      app: :addict,
+      version: "0.3.0",
+      elixir: "~> 1.2",
+      description: description(),
+      package: package(),
+      docs: &docs/0,
+      deps: deps()
+    ]
   end
 
   def application do
-    [applications: applications(Mix.env)]
+    [applications: applications(Mix.env())]
   end
 
   defp applications(:test) do
@@ -24,24 +26,26 @@ defmodule Addict.Mixfile do
   end
 
   defp deps do
-    [{:cowboy, "~> 1.0"},
-     {:phoenix, "~> 1.1"},
-     {:ecto, "~> 2.0"},
-     {:comeonin, "~> 2.1" },
-     {:mailgun, "~> 0.1"},
-     {:mock, "~> 0.1.3", only: :test},
-     {:postgrex, "~> 0.11", only: :test},
-     {:earmark, "~> 0.2", only: :dev},
-     {:ex_doc, "~> 0.11", only: :dev}]
+    [
+      {:cowboy, "~> 1.0"},
+      {:phoenix, "~> 1.1"},
+      {:ecto, "~> 2.0"},
+      {:comeonin, "~> 2.1"},
+      {:mailgun, "~> 0.1"},
+      {:mock, "~> 0.1.3", only: :test},
+      {:postgrex, "~> 0.11", only: :test},
+      {:earmark, "~> 0.2", only: :dev},
+      {:ex_doc, "~> 0.11", only: :dev}
+    ]
   end
 
   defp package do
     [
-         files: ["lib", "boilerplate", "docs", "mix.exs", "README*", "LICENSE*", "configs*"],
-         contributors: ["Nizar Venturini"],
-         maintainers: ["Nizar Venturini"],
-         licenses: ["MIT"],
-         links: %{"GitHub" => "https://github.com/trenpixster/addict"}
+      files: ["lib", "boilerplate", "docs", "mix.exs", "README*", "LICENSE*", "configs*"],
+      contributors: ["Nizar Venturini"],
+      maintainers: ["Nizar Venturini"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/meltwater/addict"}
     ]
   end
 
@@ -54,8 +58,6 @@ defmodule Addict.Mixfile do
 
   defp docs do
     {ref, 0} = System.cmd("git", ["rev-parse", "--verify", "--quiet", "HEAD"])
-    [source_ref: ref,
-     main: "readme",
-     extras: ["README.md","configs.md"]]
-    end
+    [source_ref: ref, main: "readme", extras: ["README.md", "configs.md"]]
+  end
 end
